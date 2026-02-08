037f6a69e960cf82502faca9f4cdef8e2c535a60
tcyeee <tcyeee@outlook.com>
2026-02-08 15:25:25 +0800

    feat(icons): add feather icon set and replace inline SVGs

--- pages/index/profile-content.vue
+++ pages/index/profile-content.vue
@@ -9,7 +9,7 @@
         <view class="avatar-wrapper">
           <image class="avatar" :src="userInfo.avatar" mode="aspectFill"></image>
           <view class="edit-badge">
-            <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTQiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTEgNEg0YTIgMiAwIDAgMC0yIDJ2MTRhMiAyIDAgMCAwIDIgMmgxNGEyIDIgMCAwIDAgMi0ydi03IiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48cGF0aCBkPSJNMTguNSAyLjVhMi4xMjEgMi4xMjEgMCAwIDEgMyAzTDEyIDE1bC00IDEgMS00IDkuNS05LjV6IiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48L3N2Zz4=" style="width: 28rpx; height: 28rpx;"></image>
+            <view class="icon--feather--edit" style="width: 28rpx; height: 28rpx; color: white;"></view>
           </view>
         </view>
         <text class="user-name">{{ userInfo.name }}</text>
@@ -25,7 +25,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(255, 179, 0, 0.1)">
               <!-- Sun Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSI1IiBzdHJva2U9IiNmZmIzMDAiIHN0cm9rZS13aWR0aD0iMiIgLz48cGF0aCBkPSJNMTIgMXYyTTEyIDIxdjJNNC4yMiA0LjIybDEuNDIgMS40Mk0xOC4zNiAxOC4zNmwxLjQyIDEuNDJNMSAxMmgyTTIxIDEyaDJNNC4yMiAxOS43OGwxLjQyLTEuNDJNMTguMzYgNS42NGwxLjQyLTEuNDIiIHN0cm9rZT0iI2ZmYjMwMCIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIC8+PC9zdmc+" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--sun" style="width: 40rpx; height: 40rpx; color: #ffb300;"></view>
             </view>
             <text class="item-text">Dark Mode</text>
           </view>
@@ -40,7 +40,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(157, 101, 255, 0.1)">
               <!-- Bell Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTggOEE2IDYgMCAwMDYgOGMwIDctMyA5LTMgOWgxOHMtMy0yLTMtOSIgc3Ryb2tlPSIjOWQ2NWZmIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48cGF0aCBkPSJNMTMuNzMgMjFhMiAyIDAgMDEtMy40NiAwIiBzdHJva2U9IiM5ZDY1ZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--bell" style="width: 40rpx; height: 40rpx; color: #9d65ff;"></view>
             </view>
             <text class="item-text">Notification Settings</text>
           </view>
@@ -52,7 +52,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(43, 134, 255, 0.1)">
               <!-- List Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNOCA2aDEzTTggMTJoMTNNOCAxOGgxM00zIDZoLjAxTTMgMTJoLjAxTTMgMThoLjAxIiBzdHJva2U9IiMyYjg2ZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--list" style="width: 40rpx; height: 40rpx; color: #2b86ff;"></view>
             </view>
             <text class="item-text">Questions Per Session</text>
           </view>
@@ -67,7 +67,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(0, 210, 106, 0.1)">
               <!-- Globe Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxMCIgc3Ryb2tlPSIjMDBkMjZhIiBzdHJva2Utd2lkdGg9IjIiIC8+PHBhdGggZD0iTTIgMTJoMjBNMTIgMmExNS4zIDE1LjMgMCAwMTQgMTAgMTUuMyAxNS4zIDE1LjMgMCAwMS00IDEwIDE1LjMgMTUuMyAwIDAxNC0xMCAxNS4zIDE1LjMgMCAwMTQtMTB6IiBzdHJva2U9IiMwMGQyNmEiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--globe" style="width: 40rpx; height: 40rpx; color: #00d26a;"></view>
             </view>
             <text class="item-text">App Language</text>
           </view>
@@ -85,7 +85,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(255, 107, 53, 0.1)">
               <!-- Shield Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTIgMjJzOC00IDgtMTBWNWwtOC0zLTggM3Y3YzAgNiA4IDEwIDggMTB6IiBzdHJva2U9IiNmZjZiMzUiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--shield" style="width: 40rpx; height: 40rpx; color: #ff6b35;"></view>
             </view>
             <text class="item-text">Privacy Policy</text>
           </view>
@@ -97,7 +97,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(0, 191, 165, 0.1)">
               <!-- File Text Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTQgMkg2YTIgMiAwIDAgMC0yIDJ2MTZhMiAyIDAgMCAwIDIgMmgxMmEyIDIgMCAwIDAgMi0yVjh6IiBzdHJva2U9IiMwMEJGQTUiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjxwYXRoIGQ9Ik0xNCAydjZoNiIgc3Ryb2tlPSIjMDBCRkE1IiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48cGF0aCBkPSJNMTYgMTNIOCIgc3Ryb2tlPSIjMDBCRkE1IiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48cGF0aCBkPSJNMTYgMTdIOCIgc3Ryb2tlPSIjMDBCRkE1IiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgLz48cGF0aCBkPSJNMTAgOUg4IiBzdHJva2U9IiMwMEJGQTUiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--file-text" style="width: 40rpx; height: 40rpx; color: #00BFA5;"></view>
             </view>
             <text class="item-text">Terms of Service</text>
           </view>
@@ -109,7 +109,7 @@
           <view class="item-left">
             <view class="icon-box" style="background-color: rgba(139, 155, 180, 0.1)">
               <!-- Info Icon -->
-              <image src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxMCIgc3Ryb2tlPSIjOGI5YmI0IiBzdHJva2Utd2lkdGg9IjIiIC8+PHBhdGggZD0iTTEyIDE2di00TTEyIDhoLjAxIiBzdHJva2U9IiM4YjliYjQiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiAvPjwvc3ZnPg==" style="width: 40rpx; height: 40rpx;"></image>
+              <view class="icon--feather--info" style="width: 40rpx; height: 40rpx; color: #8b9bb4;"></view>
             </view>
             <text class="item-text">Contact Us</text>
           </view>
